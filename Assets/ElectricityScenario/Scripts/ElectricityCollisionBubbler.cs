1942cbd9c9d31921cb69ef5821fa2f0fec621486
Terbau <40792825+Terbau@users.noreply.github.com>
2025-03-18 10:01:11 +0100

    fix: add HapticManager to ElectricityScene

--- Assets/ElectricityScenario/Scripts/ElectricityCollisionBubbler.cs
+++ Assets/ElectricityScenario/Scripts/ElectricityCollisionBubbler.cs
@@ -5,23 +5,23 @@ public class ElectricityCollisionBubbler : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnTriggerEnter(Collider collider)
     {
-        if (!transform.parent.TryGetComponent<ElectricityManager>(out var electricityScript))
+        if (!transform.parent.TryGetComponent<ElectricityManager>(out var electricityManager))
         {
-            Debug.LogError("ElectricityCollisionBubbler: ElectricityScript not found in parent");
+            Debug.LogError("ElectricityCollisionBubbler: ElectricityManager not found in parent");
             return;
         }
 
-        electricityScript.OnTriggerEnterFromChild(transform, collider);      
+        electricityManager.OnTriggerEnterFromChild(transform, collider);      
     }
 
     void OnTriggerExit(Collider collider)
     {
-        if (!transform.parent.TryGetComponent<ElectricityManager>(out var electricityScript))
+        if (!transform.parent.TryGetComponent<ElectricityManager>(out var electricityManager))
         {
-            Debug.LogError("ElectricityCollisionBubbler: ElectricityScript not found in parent");
+            Debug.LogError("ElectricityCollisionBubbler: ElectricityManager not found in parent");
             return;
         }
 
-        electricityScript.OnTriggerExitFromChild(transform, collider);
+        electricityManager.OnTriggerExitFromChild(transform, collider);
     }
 }
