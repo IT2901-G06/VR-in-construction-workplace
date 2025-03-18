3482d5e2dae5b82dd660fad96a39537a1a11245e
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2025-03-18 21:06:56 +0100

    fix!: Updated body part trigger scripts to correct boxes

--- Assets/FallingObjects/Scripts/BodyTriggerController.cs
+++ Assets/FallingObjects/Scripts/BodyTriggerController.cs
@@ -16,10 +16,35 @@ public class BodyTriggerController : MonoBehaviour
         Head
     }
 
+    private float _initialHeightProportion;
+    private float _initialCameraYCenter;
+    private float _initialCenterProportion;
+    private Vector3 _initialCenter;
+
+    void Start()
+    {
+        _initialCenter = GetComponent<BoxCollider>().center;
+        _initialHeightProportion = GetComponent<BoxCollider>().size.y / GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>().height;
+        _initialCameraYCenter = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>().center.y;
+        _initialCenterProportion = (_initialCameraYCenter + _initialCenter.y) / GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>().height;
+    }
+
     void Update()
     {
         Quaternion cameraRotation = GameObject.FindGameObjectWithTag("MainCamera").transform.rotation;
-        transform.rotation = new Quaternion(0, cameraRotation.y, 0, cameraRotation.w);
+        transform.rotation = new(0, cameraRotation.y, 0, cameraRotation.w);
+
+        CharacterController cameraCharController = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
+
+        float newHeight = _initialHeightProportion * cameraCharController.height;
+        Vector3 currentSize = GetComponent<BoxCollider>().size;
+        GetComponent<BoxCollider>().size = new(currentSize.x, newHeight, currentSize.z);
+
+        float newCenter = _initialCenterProportion * cameraCharController.height - _initialCameraYCenter + cameraCharController.skinWidth;
+        GetComponent<BoxCollider>().center = new(_initialCenter.x, newCenter, _initialCenter.z);
+        
+        Vector3 cameraPos = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+        transform.position = new(cameraPos.x, transform.position.y, cameraPos.z);
     }
 
     private void OnTriggerEnter(Collider other)
