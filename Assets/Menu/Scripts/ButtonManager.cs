853c5c5bddbf1faa6f381a28297fada3cc0b2843
Peter.Ehrhardt <peh014@uib.no>
2025-03-25 14:28:59 +0100

    feat: :sparkles: Added simple script for testing purposes + minor visual changes

---
+++ Assets/Menu/Scripts/ButtonManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public void HandleGloveButtonPress()
+    {
+        Debug.Log("Glove Button Pressed");
+    }
+
+    public void HandleSuitButtonPress()
+    {
+        Debug.Log("Suit Button Pressed");
+    }
+}
