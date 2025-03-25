b971e0dbe8fc5e7ee4bf0313356cd216ecede21e
Peter.Ehrhardt <peh014@uib.no>
2025-03-25 14:59:55 +0100

    feat: :sparkles: Add logic to fire bhaptic events on button press

--- Assets/Menu/Scripts/ButtonManager.cs
+++ Assets/Menu/Scripts/ButtonManager.cs
@@ -6,10 +6,32 @@ public class ButtonManager : MonoBehaviour
     public void HandleGloveButtonPress()
     {
         Debug.Log("Glove Button Pressed");
+
+        HapticController hapticController = HapticController.Instance;
+
+        if (hapticController != null)
+        {
+            MotorEvent motorEvent = BhapticsEventCollection.GloveFingersLeft;
+
+            int motorStrength = 75;
+
+            hapticController.RunMotors(motorEvent, motorStrength, hapticController.GetSingleEventMotorRunTimeMs());
+        }
     }
 
     public void HandleSuitButtonPress()
     {
         Debug.Log("Suit Button Pressed");
+
+        HapticController hapticController = HapticController.Instance;
+
+        if (hapticController != null)
+        {
+            MotorEvent motorEvent = BhapticsEventCollection.VestFront;
+
+            int motorStrength = 75;
+
+            hapticController.RunMotors(motorEvent, motorStrength, hapticController.GetSingleEventMotorRunTimeMs());
+        }
     }
 }
