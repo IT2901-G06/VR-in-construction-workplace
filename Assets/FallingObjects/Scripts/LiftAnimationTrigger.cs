507ad183f02d38de6944ec333de17073e5ef9274
Karl Theodor Ladegård Beck <ktbeck@stud.ntnu.no>
2025-03-27 21:45:38 +0100

    feat: create script for triggering animation

---
+++ Assets/FallingObjects/Scripts/LiftAnimationTrigger.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using BNG;
+
+public class LiftAnimationTrigger : MonoBehaviour
+{
+
+    private Animator craneAnimator;
+    private Animator crateWithFallingObjectsAnimator;
+
+    private GameObject Player;
+
+    //private Grabbable grabbable;  // Vet ikke om denne kanskje er riktig for BNG Framework sin Player. Spaken fra BNG har i hvert fall denne komponenten.
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        craneAnimator = GameObject.FindGameObjectWithTag("Crane").GetComponent<Animator>();
+        crateWithFallingObjectsAnimator = GameObject.FindGameObjectWithTag("CrateWithFallingObjects").GetComponent<Animator>();
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+        //grabbable = gameObject.GetComponent<Grabbable>(); // Mulig man kan bruke denne for å sjekke om spaken er grepet av spilleren med "grabbable.BeingHeld", men usikker.
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            craneAnimator.SetTrigger("PullLiftLeverCrane");
+            crateWithFallingObjectsAnimator.SetTrigger("PullLiftLeverBoxes");
+            Debug.Log("Lift lever pulled");
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
