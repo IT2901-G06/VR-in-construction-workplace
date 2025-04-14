5eff4b5be1fb5362e08b182a2eb6d961d833a73c
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2025-04-14 13:19:24 +0200

    chore: add comment for clarification

--- Assets/FallingObjects/Scripts/BoxSnapZoneScript.cs
+++ Assets/FallingObjects/Scripts/BoxSnapZoneScript.cs
@@ -6,6 +6,8 @@ public class BoxSnapZoneScript : MonoBehaviour
     public void AddBox()
     {
         BoxStackingController.Instance.IncrementStackedBoxes(GetComponent<SnapZone>().HeldItem);
+
+        // Disable ring helper on snap zone
         transform.GetChild(0).gameObject.SetActive(false);
     }
 }
