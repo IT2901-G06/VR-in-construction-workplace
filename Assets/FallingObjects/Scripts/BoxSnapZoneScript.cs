078cebe4cbce6a2c3f7a3600d7347713e22d58d1
Ola Munthe Vassbotn <93545174+olamva@users.noreply.github.com>
2025-04-15 17:34:43 +0200

    Add more items to stack on pallet (#30)
    * feat: Swapped out two of the boxes for more items
    
    Now the user is expected to place items in two different stages, on top of eachother
    
    * fix: correct prefab name

--- Assets/FallingObjects/Scripts/BoxSnapZoneScript.cs
+++ Assets/FallingObjects/Scripts/BoxSnapZoneScript.cs
@@ -5,7 +5,7 @@ public class BoxSnapZoneScript : MonoBehaviour
 {
     public void AddBox()
     {
-        BoxStackingController.Instance.IncrementStackedBoxes(GetComponent<SnapZone>().HeldItem);
+        StackingController.Instance.IncrementStackedBoxes(GetComponent<SnapZone>().HeldItem);
 
         // Disable ring helper on snap zone
         transform.GetChild(0).gameObject.SetActive(false);
