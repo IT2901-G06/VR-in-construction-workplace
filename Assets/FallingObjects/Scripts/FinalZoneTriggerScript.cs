d730df6e40ff23b72ff6bea8658f0c292eb929c7
Ola Munthe Vassbotn <93545174+olamva@users.noreply.github.com>
2025-04-28 11:56:48 +0200

    Falling Objects: Migrate to meta rig (#37)
    * feat: adjust body part triggers
    
    * feat: change to meta snap zones on first stage
    
    still not working fully tho
    
    * more progress
    
    * fix: meta shit
    
    * add teleports
    
    * remove temp
    
    * fix
    
    * fix fix
    
    * kys
    
    * fix!: this shit
    
    ---------
    
    Co-authored-by: Karl Theodor Ladegård Beck <ktbeck@stud.ntnu.no>

--- Assets/FallingObjects/Scripts/FinalZoneTriggerScript.cs
+++ Assets/FallingObjects/Scripts/FinalZoneTriggerScript.cs
@@ -4,7 +4,7 @@ public class FinalZoneTriggerScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        other.TryGetComponent(out CharacterController characterController);
+        other.TryGetComponent(out Oculus.Interaction.Locomotion.CharacterController characterController);
         if (characterController != null)
         {
             GameObject.Find("NPCSpawner").GetComponent<PeterTheExplainer>().SetPlayerReachedSafeZone(true);
@@ -13,7 +13,7 @@ public class FinalZoneTriggerScript : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        other.TryGetComponent(out CharacterController characterController);
+        other.TryGetComponent(out Oculus.Interaction.Locomotion.CharacterController characterController);
         if (characterController != null)
         {
             GameObject.Find("NPCSpawner").GetComponent<PeterTheExplainer>().SetPlayerReachedSafeZone(false);
